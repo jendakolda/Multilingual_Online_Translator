e3a176f64d0ac52007dcbefebfa0e96186e58e70
jkolda <jkolda@casaleproject.cz>
2020-11-05 14:05:34 +0100

    Stage 4 completed

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -3,31 +3,30 @@ from bs4 import BeautifulSoup
 
 
 class Translator(object):
-    pairs = {'en': 'french-english', 'fr': 'english-french'}
-    lang_shorts = {'en': 'English', 'fr': 'French'}
+    langs = dict(enumerate(['Arabic', 'German', 'English', 'Spanish', 'French',
+                            'Hebrew', 'Japanese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish'],
+                           start=1))
     user_agent = 'Mozilla/5.0'
 
     def __init__(self):
-        # self.language = input('Type "en" if you want to translate from French into English,'
-        #                       ' or "fr" if you want to translate from English into French:')
-        # self.word = input('Type the word you want to translate:')
-        self.language = 'fr'
-        self.word = 'hello'
-        print(f'You chose "{self.language}" as a language to translate "{self.word}".')
+        print('Hello, you\'re welcome to the translator. Translator supports: ')
+        print(*[str(k) + '. ' + v for k, v in Translator.langs.items()], sep='\n')
+        # print(f'You chose "{self.language}" as a language to translate "{self.word}".')
+        self.src_lang = Translator.langs[int(input('Type the number of your language:\n '))]
+        self.tran_lang = Translator.langs[int(input('Type the number of language you want to translate to: \n'))]
+        self.word = input('Type the word you want to translate:\n')
 
     def form_request(self):
-        return f'https://context.reverso.net/translation/{Translator.pairs[self.language]}/{self.word}'
+        return f'https://context.reverso.net/translation/{self.src_lang.lower()}-{self.tran_lang.lower()}/{self.word}'
 
     def main(self):
         response = requests.get(Translator.form_request(self), headers={'User-Agent': Translator.user_agent})
         web_data = BeautifulSoup(response.content, 'lxml')
         words = web_data.find('div', attrs={'id': 'translations-content'})
         translations = words.find_all('a')
-        # translations = [word.text.replace(' ', '').replace('\n', '') for word in words]
         synonyms = list()
         for word in translations:
             synonyms.append(word.text.strip())
-
 
         src_phrases = web_data.find_all("div", {"class": "src ltr"})
         tran_phrases = web_data.find_all("div", {"class": "trg ltr"})
@@ -36,11 +35,11 @@ class Translator(object):
         lst_tran_phrases = [phrase.text.strip() for phrase in tran_phrases]
 
         print(response.status_code, 'OK\n' if response else 'Denied')
-        print(f'Context examples:\n\n{Translator.lang_shorts[self.language]} Translations:')
+        print(f'Context examples:\n\n{self.tran_lang} Translations:')
         print(*synonyms[:5], sep='\n')
 
-        print(f'\n{Translator.lang_shorts[self.language]} Examples:\n')
-        print(*[lst_src_phrases[i] + '\n' + lst_tran_phrases[i] for i in range(5)], sep='\n')
+        print(f'\n{self.tran_lang} Examples:\n')
+        print(*[lst_src_phrases[i] + '\n' + lst_tran_phrases[i] + '\n' for i in range(5)], sep='\n')
 
 
 Translator().main()
