4b4a964e720d0829583907c67ae1ef3d69e62e7f
jkolda <jkolda@casaleproject.cz>
2020-11-12 21:16:29 +0100

    Stage 5 completed

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -12,21 +12,49 @@ class Translator(object):
         print('Hello, you\'re welcome to the translator. Translator supports: ')
         print(*[str(k) + '. ' + v for k, v in Translator.langs.items()], sep='\n')
         # print(f'You chose "{self.language}" as a language to translate "{self.word}".')
-        self.src_lang = Translator.langs[int(input('Type the number of your language:\n '))]
-        self.tran_lang = Translator.langs[int(input('Type the number of language you want to translate to: \n'))]
-        self.word = input('Type the word you want to translate:\n')
+        while True:
+            try:
+                self.src_lang = Translator.langs[int(input('Type the number of your language:\n '))]
+                break
+            except KeyError:
+                print('Inserted value has to be 1-13.')
 
-    def form_request(self):
-        return f'https://context.reverso.net/translation/{self.src_lang.lower()}-{self.tran_lang.lower()}/{self.word}'
+        while True:
+            lang_num = int(input('Type the number of a language you want to translate to or \'0\''
+                                 ' to translate to all languages:\n'))
+            if lang_num == 0:
+                self.length = 1
+                self.tran_lang = list(v for v in Translator.langs.values())
+                # print(self.tran_lang)
+                break
+            elif 1 <= lang_num <= 13:
+                self.tran_lang = [Translator.langs[lang_num]]
+                self.length = 5
+                break
+            else:
+                print('Inserted value has to be 0-13.')
+
+        self.word = input('Type the word you want to translate:\n')
+        self.output_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
+
+    def form_request(self, lang):
+        # return f'https://context.reverso.net/translation/{self.src_lang.lower()}-{self.tran_lang.lower()}/{self.word}'
+        return f'https://context.reverso.net/translation/{self.src_lang.lower()}-{lang.lower()}/{self.word}'
 
     def main(self):
-        response = requests.get(Translator.form_request(self), headers={'User-Agent': Translator.user_agent})
+        self.output_file = open(f'{self.word}.txt', 'w', encoding='utf-8')
+        for lang in self.tran_lang:
+            self.get_translation(lang)
+        self.output_file.close()
+
+    def get_translation(self, lang):
+        if self.src_lang.lower() == lang.lower():
+            return
+        response = requests.get(Translator.form_request(self, lang), headers={'User-Agent': Translator.user_agent})
         web_data = BeautifulSoup(response.content, 'lxml')
         words = web_data.find('div', attrs={'id': 'translations-content'})
         translations = words.find_all('a')
-        synonyms = list()
-        for word in translations:
-            synonyms.append(word.text.strip())
+        synonyms = [translation.text.strip() for translation in translations]
 
         src_phrases = web_data.find_all("div", {"class": "src ltr"})
         tran_phrases = web_data.find_all("div", {"class": "trg ltr"})
@@ -34,12 +62,19 @@ class Translator(object):
         lst_src_phrases = [phrase.text.strip() for phrase in src_phrases]
         lst_tran_phrases = [phrase.text.strip() for phrase in tran_phrases]
 
-        print(response.status_code, 'OK\n' if response else 'Denied')
-        print(f'Context examples:\n\n{self.tran_lang} Translations:')
-        print(*synonyms[:5], sep='\n')
+        # print(response.status_code, 'OK\n' if response else 'Denied')
+        print(f'Context examples:\n\n{lang} Translations:')
+        self.output_file.write(f'\n\n{lang} Translations:\n')
+        print(*synonyms[:self.length], sep='\n')
+        self.output_file.writelines(*synonyms[:self.length])
 
-        print(f'\n{self.tran_lang} Examples:\n')
-        print(*[lst_src_phrases[i] + '\n' + lst_tran_phrases[i] + '\n' for i in range(5)], sep='\n')
+        print(f'\n{lang} Examples:\n')
+        self.output_file.write(f'\n\n{lang} Example:\n')
+        if tran_phrases:
+            print(*[lst_src_phrases[i] + '\n' + lst_tran_phrases[i] + '\n' for i in range(self.length)], sep='\n')
+            for i in range(self.length):
+                self.output_file.write(lst_src_phrases[i] + '\n')
+                self.output_file.write(lst_tran_phrases[i] + '\n')
 
 
 Translator().main()
